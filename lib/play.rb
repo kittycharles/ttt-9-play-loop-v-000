341ae6286d61475265a12be8d446d8811747fd60
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-04 18:10:08 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,10 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-turn(board)
-  board = 0
-  until board = 9
+turn(board) = 0
+  until turn(board) = 9
   puts "X"
-  board += 1
+  turn(board) += 1
  end
 end
